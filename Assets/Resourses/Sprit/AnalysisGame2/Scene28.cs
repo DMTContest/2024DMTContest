88415ecc85ae735bb9d78c9d586f1b44f0b61403
liyiting1104 <139786376+liyiting1104@users.noreply.github.com>
2024-08-08 19:02:04 +0800

    钟表游戏修改

--- Assets/Resourses/Sprit/AnalysisGame2/Scene28.cs
+++ Assets/Resourses/Sprit/AnalysisGame2/Scene28.cs
@@ -94,10 +94,12 @@ public class Scene28 : MonoBehaviour
         {
             MoveNum -= 1;
             Minute.transform.RotateAround(Vector3.zero, Vector3.forward, rotationSpeed);
+            Hour.transform.RotateAround(Vector3.zero, Vector3.forward, rotationSpeed/12);
             MinuteNum -= 1;
             if (MinuteNum < 0)
             {
-                Hour.transform.RotateAround(Vector3.zero, Vector3.forward, rotationSpeed);
+                Hour.transform.RotateAround(Vector3.zero, Vector3.forward, rotationSpeed/12);
+
                 MinuteNum = 11;
             }
         }
@@ -105,10 +107,11 @@ public class Scene28 : MonoBehaviour
         {
             MoveNum += 1;
             Minute.transform.RotateAround(Vector3.zero, Vector3.forward, -rotationSpeed);
+            Hour.transform.RotateAround(Vector3.zero, Vector3.forward, -rotationSpeed/12);
             MinuteNum += 1;
             if (MinuteNum == 12)
             {
-                Hour.transform.RotateAround(Vector3.zero, Vector3.forward, -rotationSpeed);
+                Hour.transform.RotateAround(Vector3.zero, Vector3.forward, -rotationSpeed/12);
                 MinuteNum = 0;
             }
         }
